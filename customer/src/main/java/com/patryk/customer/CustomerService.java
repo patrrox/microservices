d55472c9f4b029d57fc0f221027ccb2db5c55428
Patryk Ostaszewski <patryk.ostaszewski@gmail.com>
2022-11-21 13:41:52 +0100

    SpringEureka Server for project @LoadBalanced

--- customer/src/main/java/com/patryk/customer/CustomerService.java
+++ customer/src/main/java/com/patryk/customer/CustomerService.java
@@ -24,7 +24,7 @@ public class CustomerService {
         customerRepository.saveAndFlush(customer);
         //todo: check if fraudster
         FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
-                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                "http://FRAUD/api/v1/fraud-check/{customerId}",
                 FraudCheckResponse.class,
                 customer.getId()
         );
--- fraud/src/main/java/com/patryk/fraud/FraudApplication.java
+++ fraud/src/main/java/com/patryk/fraud/FraudApplication.java
@@ -3,8 +3,10 @@ package com.patryk.fraud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class FraudApplication {
 
 
