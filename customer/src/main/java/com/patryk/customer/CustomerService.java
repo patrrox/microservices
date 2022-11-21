0051e3de107d9528a8dd28fac9ac8cc8d4d8db86
Patryk Ostaszewski <patryk.ostaszewski@gmail.com>
2022-11-21 10:56:45 +0100

    CustomerService, model and controller, db

--- customer/src/main/java/com/patryk/customer/CustomerService.java
+++ customer/src/main/java/com/patryk/customer/CustomerService.java
@@ -4,16 +4,16 @@ package com.patryk.customer;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService() {
+public record CustomerService(CustomerRepository customerRepository) {
 
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
                 .lastName(request.lastName())
+                .email(request.email())
                 .build();
-
         // todo: check if email valid
         // todo: check if email not taken
-        // todo: store customer in db
+        customerRepository.save(customer);
     }
 }
